09a80c2bec99bbf1b3f0d4d993b182fed3f30976
chenxu-yang <cy2554@columbia.edu>
2020-03-21 18:26:42 -0400

    implement front-end with react

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import 'tachyons';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
+const particlesOptions={
+  particles:{
+    number:{
+      value:100,
+      density:{
+        enable:true,
+        value_area:800
+      }
+    }
+  }
+}
+
+function App() {
+  return (
+
+    <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+
+        {/*<FaceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
